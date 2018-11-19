e52ab49359af2b9132048ed27640c69651f81977
Gennadiy <g.abashkin@petrovich.ru>
2018-11-19 20:11:19 +0300

    Added raw frame of function

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const http = require('http');
 
-function sendResponse() {
+let iterations = 10;
+let timerEndTime = 15000;
 
+
+function viewTimeInConsole() {
+  let startTime = new Date().getTime();
+  let timerIds = [];
+
+  for (let i = 0; i <= iterations; i++) {
+    let timerId = setInterval(() => {
+      console.log(returnTime())
+    }, 5000);
+  }
+
+
+
+
+  setTimeout(() => {
+    clearInterval(timerId);
+    return;
+  }, timerTime)
+}
+
+function returnTime() {
+    return new Date().toISOString();
 }
 
 http.createServer(function(request, response) {
     if (request.method == 'GET') {
-        sendResponse();
+        viewTimeInConsole();
     } else {
         console.log('request method is not supported, send GET request.');
     }
